33e4a02cd4422ef24bb77159b61ad88eeef4d047
User from script <S3dk0vAG@gmail.com>
2021-11-16 20:43:15 +0300

    init project

---
+++ src/main/java/by/anthony/traiderstat/TraiderstatApplication.java
@@ -0,0 +1,13 @@
+package by.anthony.traiderstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraiderstatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraiderstatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/anthony/traiderstat/TraiderstatApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anthony.traiderstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraiderstatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
